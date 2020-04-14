278211e1422aa405da7550f9c052efa475eb4e93
ythuang13 <60416834+ythuang13@users.noreply.github.com>
2020-04-14 11:49:01 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font as tkFont
 import math
 
 
@@ -46,106 +47,104 @@ def btnDelete():
 
 def enlarge():
     global fontSize
-    fontSize += 5
-    # cal.update()
+    fontSize.configure(size = fontSize.cget('size') + 1)
 
 
 def shrink():
     global fontSize
-    fontSize -= 5
-    cal.update()
+    fontSize.configure(size = fontSize.cget('size') - 1)
 
 
 def main():
-    global operator, text_input
+    global operator, text_input, fontSize
     cal = Tk()
     cal.title("Calculator")
     operator = ""
     text_input = StringVar()
-    fontSize = 13
+    fontSize = tkFont.Font(family="ariel", size=15, weight="bold")
     xPad = 16
     yPad = 16
 
-    txtDisplay = Entry(cal, font=("ariel", fontSize + 5),
+    txtDisplay = Entry(cal, font=fontSize,
                        textvariable=text_input,
                        bd=15, insertwidth=3, bg="powder blue",
                        justify='right').grid(columnspan=5)
     # 7, 8 ,9
-    btn7 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn7 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="7",
                   command=lambda: btnClick(7)).grid(row=1, column=0)
-    btn8 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn8 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="8",
                   command=lambda: btnClick(8)).grid(row=1, column=1)
-    btn9 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn9 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="9",
                   command=lambda: btnClick(9)).grid(row=1, column=2)
     # 4, 5, 6
-    btn4 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn4 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="4",
                   command=lambda: btnClick(4)).grid(row=2, column=0)
-    btn5 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn5 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="5",
                   command=lambda: btnClick(5)).grid(row=2, column=1)
-    btn6 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn6 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="6",
                   command=lambda: btnClick(6)).grid(row=2, column=2)
     # 1, 2, 3
-    btn1 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn1 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="1",
                   command=lambda: btnClick(1)).grid(row=3, column=0)
-    btn2 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn2 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="2",
                   command=lambda: btnClick(2)).grid(row=3, column=1)
-    btn3 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn3 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="3",
                   command=lambda: btnClick(3)).grid(row=3, column=2)
     # C, 0, =
-    btn1Clear = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn1Clear = Button(cal, font=fontSize, padx=16, pady=16,
                        bd=8, fg="black", text="C",
                        command=btnClearDisplay).grid(row=4, column=0)
-    btn0 = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btn0 = Button(cal, font=fontSize, padx=16, pady=16,
                   bd=8, fg="black", text="0",
                   command=lambda: btnClick(0)).grid(row=4, column=1)
-    btnEquals = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnEquals = Button(cal, font=fontSize, padx=16, pady=16,
                        bd=8, fg="black", text="=",
                        command=btnEqualsInput).grid(row=4, column=2)
     # + , - , *, /
-    btnAdd = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnAdd = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="+",
                     command=lambda: btnClick("+")).grid(row=1, column=3)
-    btnSub = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnSub = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="-",
                     command=lambda: btnClick("-")).grid(row=2, column=3)
-    btnMul = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnMul = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="*",
                     command=lambda: btnClick("*")).grid(row=3, column=3)
-    btnDiv = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnDiv = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="/",
                     command=lambda: btnClick("/")).grid(row=4, column=3)
 
     # sqrt, ^, ^2, back
-    btnSqrt = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnSqrt = Button(cal, font=fontSize, padx=16, pady=16,
                      bd=8, fg="black", text="√",
                      command=btnSquareRoot).grid(row=5, column=0)
-    btnPow = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnPow = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="^",
                     command=lambda: btnClick("**")).grid(row=5, column=1)
-    btnSquare = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnSquare = Button(cal, font=fontSize, padx=16, pady=16,
                        bd=8, fg="black", text="^2",
                        command=btnSquared).grid(row=5, column=2)
-    btnDel = Button(cal, font=("ariel", fontSize), padx=16, pady=16,
+    btnDel = Button(cal, font=fontSize, padx=16, pady=16,
                     bd=8, fg="black", text="↵",
                     command=btnDelete).grid(row=5, column=3)
-    """
+    
     # enlarge, shrink
-    btnEnlarge = Button(cal, font=("ariel", fontSize), padx=xPad * 2,
+    btnEnlarge = Button(cal, font=fontSize, padx=xPad * 2,
                         pady=yPad, bd=8, fg="black", text="enlarge",
                         command=enlarge).grid(row=6, column=0, columnspan=2)
-    btnShrink = Button(cal, font=("ariel", fontSize), padx=xPad * 2, pady=yPad,
+    btnShrink = Button(cal, font=fontSize, padx=xPad * 2, pady=yPad,
                        bd=8, fg="black", text="shrink",
                        command=shrink).grid(row=6, column=2, columnspan=2)
-    """
+    
     cal.mainloop()
 
 
